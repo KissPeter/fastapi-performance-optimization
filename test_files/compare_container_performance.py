198cf6fbcbf8c8fda0b3d0d411ec42fe1c261cfe
Péter Kiss <kiss.peter@linuxadm.hu>
2022-10-01 22:56:38 +0200

    container test result postprocessing

--- test_files/compare_container_performance.py
+++ test_files/compare_container_performance.py
@@ -1,12 +1,15 @@
 import os
 import subprocess
 import shlex
+from collections import defaultdict
+
+from tabulate import tabulate
 
 from ab_wrapper.runner import Runner
 from ab_wrapper.config import Config
 from ab_wrapper.parser import Parser
 from ab_wrapper.collector import Collector
-from typing import Dict
+from typing import Dict, List
 
 
 class ABConfig(Config):
@@ -80,13 +83,21 @@ class ABRunner(Runner):
 
 
 class TestContainer:
+    DEFAULT_URI = "/items/"
 
-    def __init__(self, port: int = 8000, uri: str = "/items/"):
+    def __init__(self, port: int = 8000, uri: str = DEFAULT_URI):
         self.ab_parser = Parser()
         self.ab_collector = Collector()
         self.port = port
-        self.uri = uri if uri.startswith('/') else f"/{uri}"
+        self.uri = self._identify_uri(uri=uri)
         self.ab_raw_results = {}
+
+    def _identify_uri(self, uri):
+        if uri:
+            uri = uri if uri.startswith('/') else f"/{uri}"
+        else:
+            uri = self.DEFAULT_URI
+        return uri
 
     def _get_config(self):
         """
@@ -97,7 +108,7 @@ class TestContainer:
             "_defaults": {
                 "time": 5,
                 "clients": 100,
-                "count": 10000,
+                "count": 100,  # 10000
                 "content_type": "'application/json'",
                 "request_body": os.path.abspath(
                     os.path.join(os.path.dirname(os.path.realpath(__file__)), 'requestbody')),
@@ -124,19 +135,74 @@ class TestContainer:
 
 
 class CompareContainers:
+    TEST_RUN_PER_CONTAINER = 1
 
     def __init__(self, test_config: Dict):
         self.test_config = test_config
-        self.test_results = {}
+        self.test_results = []
 
     def run_test(self):
         for container in self.test_config:
             port = container.get('port')
             name = container.get('name')
+            uri = container.get('uri')
             baseline = container.get('name', False)
+            container['results'] = self.test_container(port=port, uri=uri)
+            self.test_results.append(container)
 
-    def test_container(self, ab_config):
+        print(self.test_results)
+
+    def sum_test_results(self, results: list) -> dict:
+        """
+        IN:
+        [{'failed_requests': 0, 'rps': 1280.72, 'time_mean': 78.081}]
+        Out (in dict)
+        | **Test attribute**    | **Test run 1** | **Test run 2** | **Test run 3** | **Average** | Difference to baseline [%] |
+        |-----------------------|----------------|----------------|----------------|-------------|----------------------------|
+        | Requests per second   | 686,21         | 689,44         | 674,9          | **683,52**  | -51,59                     |
+        | Time per request [ms] | 145,728        | 145,044        | 148,17         | **146,31**  | 34,03                      |
+        """
         pass
+
+    @staticmethod
+    def tabulate_data(headers: List[str], data: dict):
+        """
+        tabulate_data(
+            headers=["Node name", "Last activity"],
+            data=self.node_info.get_failing_nodes(),
+        )
+        :param headers:
+        :param data:
+        :return:
+        """
+        table_data = []
+        for k, v in data.items():
+            table_data.append([k, v])
+        print(tabulate(table_data, headers=headers, tablefmt="grid"))
+
+    def sum_container_results(self):
+        """
+        [{'name': 'base', 'port': 8000, 'baseline': True,
+        'results': [{'failed_requests': 0, 'rps': 1280.72, 'time_mean': 78.081}]},
+        {'name': 'app_one_base_middleware', 'port': 8001, 'baseline': False, 'results': [{'failed_requests': 0, 'rps': 917.87, 'time_mean': 108.948}]}, {'name': 'app_two_base_middlewares', 'port': 8002, 'baseline': False, 'results': [{'failed_requests': 0, 'rps': 612.07, 'time_mean': 163.379}]}]
+        :return:
+        """
+        baseline = {}
+        results = {}
+        for container in self.test_results:
+            if container.get('baseline'):
+                baseline = self.sum_container_results(container.get('results'))
+            else:
+                results = self.sum_container_results(container.get('results'))
+
+    def test_container(self, port, uri):
+        _results = []
+        for i in range(self.TEST_RUN_PER_CONTAINER):
+            print(f"{i}. of {port} / {uri}")
+            t = TestContainer(port=port, uri=uri)
+            t.run()
+            _results.append(t.get_results())
+        return _results
 
 
 test_config = [
@@ -156,11 +222,9 @@ test_config = [
         "baseline": False
     }
 ]
-# p = CompareContainers(test_config)
-# p.run_test()
-# print(ABRunner.execute_command_whole_output(cmd=['ab', '-q ', '-c 100', '-n 10000', "-T 'application/json'", '-p /home/kissp/PycharmProjects/fastapi-middlewares/test_files/requestbody', ' http://127.0.0.1:8000/items/']))
+p = CompareContainers(test_config)
+p.run_test()
 # exit()
-p = TestContainer()
-p.run()
-print("DATA?")
-print(p.get_results())
+# p = TestContainer()
+# p.run()
+# print(p.get_results())
