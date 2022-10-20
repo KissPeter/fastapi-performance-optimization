84ea496dd89839d7b33ca14df984936f443df910
Kiss Péter <kiss.peter@linuxadm.hu>
2022-10-20 09:02:18 +0200

    increase request count for threads and workers measurement

--- test_files/test_workers_and_threads.py
+++ test_files/test_workers_and_threads.py
@@ -8,55 +8,55 @@ def test_workers_and_threads():
             "name": "app_w1_t1",
             "port": 8008,
             "baseline": True,
-            "request_count": 500
+            "request_count": 2000
         },
         {
             "name": "app_w2_t1",
             "port": 8009,
             "baseline": False,
-            "request_count": 500
+            "request_count": 2000
         },
         {
             "name": "app_w3_t1",
             "port": 8010,
             "baseline": False,
-            "request_count": 500
+            "request_count": 2000
         },
         {
             "name": "app_w1_t2",
             "port": 8011,
             "baseline": False,
-            "request_count": 500
+            "request_count": 2000
         },
         {
             "name": "app_w1_t3",
             "port": 8012,
             "baseline": False,
-            "request_count": 500
+            "request_count": 2000
         },
         {
             "name": "app_w2_t2",
             "port": 8013,
             "baseline": False,
-            "request_count": 500
+            "request_count": 2000
         },
         {
             "name": "app_w2_t3",
             "port": 8014,
             "baseline": False,
-            "request_count": 500
+            "request_count": 2000
         },
         {
             "name": "app_w3_t2",
             "port": 8015,
             "baseline": False,
-            "request_count": 500
+            "request_count": 2000
         },
         {
             "name": "app_w3_t3",
             "port": 8016,
             "baseline": False,
-            "request_count": 500
+            "request_count": 2000
         }
     ]
 
