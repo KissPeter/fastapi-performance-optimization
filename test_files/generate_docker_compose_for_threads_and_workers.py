78b25c8fd3c1ad7f6045b52e1e018a891eed6f6f
Kiss Péter <kiss.peter@linuxadm.hu>
2022-10-21 15:29:20 +0200

    workers and threads run separately

--- test_files/generate_docker_compose_for_threads_and_workers.py
+++ test_files/generate_docker_compose_for_threads_and_workers.py
@@ -1,6 +1,7 @@
 import json
-import yaml
 from copy import deepcopy
+
+import yaml
 
 
 # define a custom representer for strings
@@ -10,27 +11,34 @@ def quoted_presenter(dumper, data):
 
 yaml.add_representer(str, quoted_presenter)
 
-compos = yaml.load(open('../docker-compose.yml'), Loader=yaml.Loader)
 compose_out = {"version": "3.1", "services": {}}
-print(f" Using {compos['services']['app_w3_t2']} as sample")
-sample = compos['services']['app_w3_t2']
+test_config = []
+sample = {'image': 'fastapi-performance-optimization:latest', 'cpus': 2, 'environment': {'WORKERS': 3, 'THREADS': 2},
+          'ports': ['8015:8000']}
 # {'image': 'fastapi-performance-optimization:latest', 'cpus': 2, 'ports': ['8000:8000']}
 port = 8100
 w = 0
+baseline = True
 for _ in range(1, 6):
     w += 1
-    t =  0
+    t = 0
     for _ in range(1, 6):
         t += 1
+        cont_id = f'app_w{w}_t{t}'
+
         print(f"{w} w , {t} t, {port} port")
-        _sample = sample.copy()
+        _sample = deepcopy(sample)
         _sample['ports'] = [f"{port}:8000"]
         _sample['environment']['THREADS'] = t
         _sample['environment']['WORKERS'] = w
-        compose_out['services'][f'app_w{w}_t{t}'] = _sample
+        compose_out['services'][cont_id] = _sample
+        test_config.append({"name": cont_id,
+                            "port": port,
+                            "baseline": baseline, })
         port += 1
-print(json.dumps(compose_out, indent=2, sort_keys=True))
+        baseline = False
 with open('../docker-compose_workers_and_threads.yml', 'w') as f:
     # don't add &id001
     yaml.Dumper.ignore_aliases = lambda self, data: True
     yaml.dump(compose_out, f, Dumper=yaml.Dumper, default_flow_style=False)
+print(test_config)
--- test_files/test_workers_and_threads.py
+++ test_files/test_workers_and_threads.py
@@ -2,88 +2,31 @@ import pytest
 
 from compare_container_performance import CompareContainers
 
-test_config = [
-    {
-        "name": "app_w1_t1",
-        "port": 8008,
-        "baseline": True,
-    },
-    {
-        "name": "app_w2_t1",
-        "port": 8009,
-        "baseline": False,
-    },
-    {
-        "name": "app_w3_t1",
-        "port": 8010,
-        "baseline": False,
-    },
-    {
-        "name": "app_w1_t2",
-        "port": 8011,
-        "baseline": False,
-    },
-    {
-        "name": "app_w1_t3",
-        "port": 8012,
-        "baseline": False,
-    },
-    {
-        "name": "app_w2_t2",
-        "port": 8013,
-        "baseline": False,
-    },
-    {
-        "name": "app_w2_t3",
-        "port": 8014,
-        "baseline": False,
-    },
-    {
-        "name": "app_w3_t2",
-        "port": 8015,
-        "baseline": False,
-    },
-    {
-        "name": "app_w3_t3",
-        "port": 8016,
-        "baseline": False,
-    },
-    {
-        "name": "app_w4_t1",
-        "port": 8017,
-        "baseline": False,
-    },
-    {
-        "name": "app_w4_t2",
-        "port": 8018,
-        "baseline": False
-    },
-    {
-        "name": "app_w4_t3",
-        "port": 8019,
-        "baseline": False,
-    },
-    {
-        "name": "app_w4_t4",
-        "port": 8020,
-        "baseline": False,
-    },
-    {
-        "name": "app_w1_t4",
-        "port": 8021,
-        "baseline": False
-    },
-    {
-        "name": "app_w2_t4",
-        "port": 8022,
-        "baseline": False,
-    },
-    {
-        "name": "app_w3_t4",
-        "port": 8023,
-        "baseline": False,
-    }
-]
+test_config = [{'name': 'app_w1_t1', 'port': 8100, 'baseline': True},
+               {'name': 'app_w1_t2', 'port': 8101, 'baseline': False},
+               {'name': 'app_w1_t3', 'port': 8102, 'baseline': False},
+               {'name': 'app_w1_t4', 'port': 8103, 'baseline': False},
+               {'name': 'app_w1_t5', 'port': 8104, 'baseline': False},
+               {'name': 'app_w2_t1', 'port': 8105, 'baseline': False},
+               {'name': 'app_w2_t2', 'port': 8106, 'baseline': False},
+               {'name': 'app_w2_t3', 'port': 8107, 'baseline': False},
+               {'name': 'app_w2_t4', 'port': 8108, 'baseline': False},
+               {'name': 'app_w2_t5', 'port': 8109, 'baseline': False},
+               {'name': 'app_w3_t1', 'port': 8110, 'baseline': False},
+               {'name': 'app_w3_t2', 'port': 8111, 'baseline': False},
+               {'name': 'app_w3_t3', 'port': 8112, 'baseline': False},
+               {'name': 'app_w3_t4', 'port': 8113, 'baseline': False},
+               {'name': 'app_w3_t5', 'port': 8114, 'baseline': False},
+               {'name': 'app_w4_t1', 'port': 8115, 'baseline': False},
+               {'name': 'app_w4_t2', 'port': 8116, 'baseline': False},
+               {'name': 'app_w4_t3', 'port': 8117, 'baseline': False},
+               {'name': 'app_w4_t4', 'port': 8118, 'baseline': False},
+               {'name': 'app_w4_t5', 'port': 8119, 'baseline': False},
+               {'name': 'app_w5_t1', 'port': 8120, 'baseline': False},
+               {'name': 'app_w5_t2', 'port': 8121, 'baseline': False},
+               {'name': 'app_w5_t3', 'port': 8122, 'baseline': False},
+               {'name': 'app_w5_t4', 'port': 8123, 'baseline': False},
+               {'name': 'app_w5_t5', 'port': 8124, 'baseline': False}]
 
 
 @pytest.mark.workers_and_threads
