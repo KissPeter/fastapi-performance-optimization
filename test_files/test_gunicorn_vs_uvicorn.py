474cf5b5bac38e583155ab5d6c467e09866c598c
Kiss Péter <kiss.peter@linuxadm.hu>
2022-10-24 09:32:25 +0200

    best performing gunicorn vs uvicorn

--- test_files/test_gunicorn_vs_uvicorn.py
+++ test_files/test_gunicorn_vs_uvicorn.py
@@ -2,22 +2,23 @@ import pytest
 
 from compare_container_performance import CompareContainers
 
-test_config = [{'name': 'app_gunicorn_w1_t0', 'port': 8010, 'baseline': True},
-               {'name': 'app_gunicorn_w2_t0', 'port': 8011, 'baseline': False},
-               {'name': 'app_uvicorn_w1', 'port': 8012, 'baseline': False},
-               {'name': 'app_uvicorn_w2', 'port': 8014, 'baseline': False}]
+test_config_w1 = [{'name': 'app_gunicorn_w1_t0', 'port': 8010, 'baseline': True},
+                  {'name': 'app_gunicorn_w1_t4', 'port': 8012, 'baseline': False},
+                  {'name': 'app_uvicorn_w1', 'port': 8015, 'baseline': False},
+                  ]
+
 
 @pytest.mark.gunicorn_vs_uvicorn
-def test_gunicorn_vs_uvicorn_sync():
-    p = CompareContainers(test_config)
+def test_gunicorn_vs_uvicorn_sync_w1():
+    p = CompareContainers(test_config_w1)
     p.run_test()
     p.sum_container_results()
 
 
 @pytest.mark.gunicorn_vs_uvicorn
-def test_gunicorn_vs_uvicorn_async():
+def test_gunicorn_vs_uvicorn_async_w1():
     async_test_config = []
-    for container in test_config.copy():
+    for container in test_config_w1.copy():
         container['uri'] = '/async/items'
         async_test_config.append(container)
     print(async_test_config)
@@ -26,25 +27,25 @@ def test_gunicorn_vs_uvicorn_async():
     p.sum_container_results()
 
 
+test_config_w2 = [
+    {'name': 'app_gunicorn_w2_t0', 'port': 8011, 'baseline': True},
+    {'name': 'app_gunicorn_w2_t2', 'port': 8014, 'baseline': False},
+    {'name': 'app_uvicorn_w2', 'port': 8016, 'baseline': False}
+]
+
+
 @pytest.mark.gunicorn_vs_uvicorn
-def test_gunicorn_vs_uvicorn_sync_big_json_response():
-    async_test_config = []
-    for container in test_config.copy():
-        container['uri'] = '/sync/big_json_response'
-        container['request_count'] = 1000
-        async_test_config.append(container)
-    print(async_test_config)
-    p = CompareContainers(async_test_config)
+def test_gunicorn_vs_uvicorn_sync_w2():
+    p = CompareContainers(test_config_w2)
     p.run_test()
     p.sum_container_results()
 
 
 @pytest.mark.gunicorn_vs_uvicorn
-def test_gunicorn_vs_uvicorn_async_big_json_response():
+def test_gunicorn_vs_uvicorn_async_w2():
     async_test_config = []
-    for container in test_config.copy():
-        container['uri'] = '/async/big_json_response'
-        container['request_count'] = 1000
+    for container in test_config_w2.copy():
+        container['uri'] = '/async/items'
         async_test_config.append(container)
     print(async_test_config)
     p = CompareContainers(async_test_config)
